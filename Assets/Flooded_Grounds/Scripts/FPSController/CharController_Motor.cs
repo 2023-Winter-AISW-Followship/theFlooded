0220c1ab5a7ff1a4f27fc329522150ad6dbc815b
현진혁 <guswls4928@naver.com>
2024-01-03 16:28:00 +0900

    initial

---
+++ Assets/Flooded_Grounds/Scripts/FPSController/CharController_Motor.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CharController_Motor : MonoBehaviour {
+
+	private float speed = 10.0f;
+	private float sensitivity = 400.0f;
+	private float WaterHeight = 15.5f;
+	CharacterController character;
+	public GameObject cam;
+	float moveFB, moveLR, moveUD = 0f;
+	float rotX, rotY;
+	public bool webGLRightClickRotation = true;
+	float gravity = -9.8f;
+
+
+	void Start(){
+		//LockCursor ();
+		character = GetComponent<CharacterController> ();
+		if (Application.isEditor) {
+			webGLRightClickRotation = false;
+			sensitivity = sensitivity * 1.5f;
+		}
+	}
+
+
+	void CheckForWaterHeight(){
+		if (transform.position.y < WaterHeight) {
+			gravity = 0f;
+		} else {
+			gravity = -9.8f;
+		}
+	}
+
+
+
+	void Update(){
+		moveFB = Input.GetAxis ("Horizontal") * speed;
+		moveLR = Input.GetAxis ("Vertical") * speed;
+
+		rotX = Input.GetAxis ("Mouse X") * sensitivity;
+		rotY = Input.GetAxis ("Mouse Y") * sensitivity;
+
+		//rotX = Input.GetKey (KeyCode.Joystick1Button4);
+		//rotY = Input.GetKey (KeyCode.Joystick1Button5);
+
+		if(Input.GetKeyDown (KeyCode.Space))
+		{
+            moveUD = 7.5f;
+        }
+
+		moveUD += gravity * Time.deltaTime;
+
+        Vector3 movement = new Vector3 (moveFB, moveUD, moveLR);
+
+        if (webGLRightClickRotation) {
+			if (Input.GetKey (KeyCode.Mouse0)) {
+				CameraRotation (cam, rotX, rotY);
+			}
+		} else if (!webGLRightClickRotation) {
+			CameraRotation (cam, rotX, rotY);
+		}
+
+		
+		movement = transform.rotation * movement;
+		character.Move (movement * Time.deltaTime);
+    }
+
+
+	void CameraRotation(GameObject cam, float rotX, float rotY){		
+		transform.Rotate (0, rotX * Time.deltaTime, 0);
+		cam.transform.Rotate (-rotY * Time.deltaTime, 0, 0);
+	}
+}
