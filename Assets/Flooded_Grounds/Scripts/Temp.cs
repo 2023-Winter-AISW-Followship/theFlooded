de543089ab03b21dfa782773d421526a8808547c
현진혁 <guswls4928@naver.com>
2024-01-04 15:35:00 +0900

    temp controll

---
+++ Assets/Flooded_Grounds/Scripts/Temp.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Temp : MonoBehaviour
+{
+    public GameObject point;
+    public GameObject spot;
+    void Start()
+    {
+        point.SetActive(true);
+        spot.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            point.SetActive(true);
+            spot.SetActive(false);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            point.SetActive(false);
+            spot.SetActive(true);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            if (RenderSettings.fog)
+            {
+                RenderSettings.fog = false;
+                RenderSettings.ambientLight = Color.white;
+            }
+            else
+            {
+                RenderSettings.fog = true;
+                RenderSettings.ambientLight = Color.black;
+            }
+        }
+    }
+}
