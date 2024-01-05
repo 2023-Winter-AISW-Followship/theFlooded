933f89da2cc97cfc5a954dc7c1387668a19901a3
현진혁 <guswls4928@naver.com>
2024-01-05 19:58:28 +0900

    radio use point guide

--- Assets/Flooded_Grounds/Scripts/Item/ItemDetect.cs
+++ Assets/Flooded_Grounds/Scripts/Item/ItemDetect.cs
@@ -13,32 +13,29 @@ public class ItemDetect : MonoBehaviour
 
     void Update()
     {
-        Debug.DrawLine(ray.origin, ray.origin + ray.direction * 100f, Color.red); //씬에서 내가 보고있는 방향을 표시
-
         ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
 
         if (Physics.Raycast(ray, out hit, raycastDistance)) //인식할 수 있는 범위 안에서 물체 확인
         {
-            GameObject hitObject = hit.collider.gameObject; //주변 물체의 정보를 가져옵니다.
 
-            if (hitObject.layer == 6)
+            if (hit.collider.gameObject.layer == 6)
             {   
                 path.Clear();
-                path.Append(hitObject.transform.GetChild(0).name);
+                path.Append(hit.collider.gameObject.transform.GetChild(0).name);
                 path.Append("/Axis/Pickup");
-                hitObject.transform.Find(path.ToString()).GetComponent<PickupMessage>().detected();
+                hit.collider.gameObject.transform.Find(path.ToString()).GetComponent<PickupMessage>().detected();
                 if (Input.GetKeyDown(KeySetting.key[KeyAction.INTERACTION]))
                 {
                     if(Camera.main.transform.childCount != 0)
                     {
                         return;
                     }
-                    GameObject temp = hitObject.transform.GetChild(0).gameObject;
+                    GameObject temp = hit.collider.gameObject.transform.GetChild(0).gameObject;
                     temp.transform.parent = Camera.main.transform;
                     temp.transform.localPosition = new Vector3(1, -0.7f, 1);
                     temp.transform.localEulerAngles = new Vector3(0, 0, 0);
                     temp.GetComponent<Rigidbody>().useGravity = false;
-                    Destroy(hitObject.gameObject);
+                    Destroy(hit.collider.gameObject.gameObject);
                 }
             }
         }
--- Assets/Flooded_Grounds/Scripts/Item/Radio/Radio.cs
+++ Assets/Flooded_Grounds/Scripts/Item/Radio/Radio.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class Radio : MonoBehaviour
 {
-    // Start is called before the first frame update
+    RaycastHit hit;
+    Ray ray;
+    public float raycastDistance = 5f;
+    GameObject point;
+
     void Start()
     {
         this.GetComponentInChildren<AudioSource>().enabled = false;
+        point = GameObject.Find("GameObject").transform.GetChild(0).gameObject;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Camera.main.transform.Find("Radio(Clone)") != null)
+        {
+            ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+
+            if (Physics.Raycast(ray, out hit, raycastDistance)) //인식할 수 있는 범위 안에서 물체 확인
+            {
+                if (hit.collider.gameObject.layer == 3)
+                {
+                    point.transform.position = hit.point;
+                    point.SetActive(true);
+                    Debug.Log("right");
+                }
+                else
+                {
+                    point.SetActive(false);
+                }
+            }
+            else
+            {
+                point.SetActive(false);
+            }
+        }
     }
 }
