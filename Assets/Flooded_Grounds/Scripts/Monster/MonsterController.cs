ef76ac0de714e5a6b5a54081df3ba8e6468345d0
현진혁 <guswls4928@naver.com>
2024-01-19 19:12:17 +0900

    monster recognize

--- Assets/Flooded_Grounds/Scripts/Monster/MonsterController.cs
+++ Assets/Flooded_Grounds/Scripts/Monster/MonsterController.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
+using static UnityEditor.PlayerSettings;
+using static UnityEngine.GraphicsBuffer;
 
 public class MonsterController : MonoBehaviour
 {
@@ -10,6 +13,10 @@ public class MonsterController : MonoBehaviour
 
     [SerializeField]
     private LayerMask soundLayer;
+    [SerializeField]
+    private LayerMask playerLayer;
+    [SerializeField]
+    private LayerMask obstacleLayer;
 
     private Animator animator;
     private float awakeTime = 10f;
@@ -19,20 +26,89 @@ public class MonsterController : MonoBehaviour
         animator = GetComponent<Animator>();
     }
 
-    Collider[] target = new Collider[5];
-    float max;
-    int index;
-    float distWeight;
-    AudioSource targetSound;
-    float volumePerDist;
-
-    private void Update()
+    private void Start()
     {
         
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireSphere(transform.position, monsterData.SightRecognitionDist);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, monsterData.Reach);
+
+        Vector3 rightDir = AngleToDir(transform.eulerAngles.y + monsterData.SightRecognitionAngle * 0.5f);
+        Vector3 leftDir = AngleToDir(transform.eulerAngles.y - monsterData.SightRecognitionAngle * 0.5f);
+        
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawRay(transform.position, rightDir * monsterData.SightRecognitionDist);
+        Gizmos.DrawRay(transform.position, leftDir * monsterData.SightRecognitionDist);
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawRay(transform.position, lookDir * monsterData.SightRecognitionDist);
+    }
+
+    Vector3 AngleToDir(float angle)
+    {
+        float radian = angle * Mathf.Deg2Rad;
+        return new Vector3(Mathf.Sin(radian), 0f, Mathf.Cos(radian));
+    }
+
+    Vector3 lookDir;
+    private void Update()
+    {
+        lookDir = AngleToDir(transform.eulerAngles.y);
+
+        playerRecognize();
+        soundRecognize();
+    }
+
+    void playerRecognize()
+    {
+        Collider[] player = new Collider[1];
+        float targetDist;
+
+        bool cast = Physics.OverlapSphereNonAlloc(
+            transform.position,
+            Mathf.Max(monsterData.SoundRecognitionDist, monsterData.SightRecognitionDist),
+            player,
+            playerLayer) > 0 ? true : false;
+
+        if (cast)
+        {
+            targetDist = Vector3.Distance(transform.position, player[0].transform.position);
+
+            if (targetDist < monsterData.SightRecognitionDist && (player[0].gameObject.layer == 9 || monsterData.MonsterName.Equals("gazer")))
+            {
+                Vector3 targetDir = (player[0].transform.position - transform.position).normalized;
+                float targetAngle = Mathf.Acos(Vector3.Dot(AngleToDir(transform.eulerAngles.y), targetDir)) * Mathf.Rad2Deg;
+                if (targetAngle <= monsterData.SightRecognitionAngle * 0.5f && !Physics.Raycast(transform.position, targetDir, monsterData.SightRecognitionDist, obstacleLayer))
+                {
+                    Debug.DrawLine(transform.position, player[0].transform.position, Color.red);
+                    transform.LookAt(player[0].transform);
+                    animator.SetBool("recognize", true);
+                }
+            }
+        }
+    }
+
+    void soundRecognize()
+    {
+        Collider[] targets = new Collider[5];
+        GameObject target;
+        float max;
+        int index;
+        float targetDist;
+        float distWeight;
+        AudioSource targetSound;
+        float volumePerDist;
+
         int size = Physics.OverlapSphereNonAlloc(
             transform.position,
-            monsterData.SoundRecognitionDist,
-            target,
+            Mathf.Max(monsterData.SoundRecognitionDist, monsterData.SightRecognitionDist),
+            targets,
             soundLayer);
 
         max = -1;
@@ -40,19 +116,21 @@ public class MonsterController : MonoBehaviour
 
         for (int i = 0; i < size; i++)
         {
-            distWeight = Mathf.Lerp(1.2f, 0, Vector3.Distance(transform.position, target[i].gameObject.transform.position) / monsterData.SoundRecognitionDist);
-            targetSound = target[i].GetComponent<AudioSource>();
-            //Debug.Log(gameObject.name + " " + Vector3.Distance(transform.position, target[i].gameObject.transform.position) + " " + distWeight);
+            target = targets[i].gameObject;
+            targetDist = Vector3.Distance(transform.position, target.transform.position);
+
+            distWeight = Mathf.Lerp(1.2f, 0, targetDist / monsterData.SoundRecognitionDist);
+            targetSound = target.GetComponent<AudioSource>();
             volumePerDist = targetSound.volume * distWeight;
             if (volumePerDist > 0.25f && volumePerDist > max)
             {
                 max = volumePerDist;
                 index = i;
-            }    
+            }
         }
         if (index != -1)
         {
-            transform.LookAt(target[index].transform);
+            transform.LookAt(targets[index].transform);
             animator.SetBool("recognize", true);
         }
     }
--- Assets/Flooded_Grounds/Scripts/Monster/MonsterData.cs
+++ Assets/Flooded_Grounds/Scripts/Monster/MonsterData.cs
@@ -9,13 +9,20 @@ public class MonsterData : ScriptableObject
     [SerializeField]
     private string monsterName;
     public string MonsterName {  get { return monsterName; } }
-    [SerializeField]
+
+    [SerializeField, Range(0f, 50f)]
     private int soundRecognitionDist;
     public int SoundRecognitionDist { get {  return soundRecognitionDist; } }
-    [SerializeField]
+
+    [SerializeField, Range(0f, 70f)]
     private int sightRecognitionDist;
     public int SightRecognitionDist { get { return sightRecognitionDist; } }
-    [SerializeField]
+
+    [SerializeField, Range(0f, 360f)]
+    private int sightRecognitionAngle;
+    public int SightRecognitionAngle { get { return sightRecognitionAngle; } }
+
+    [SerializeField, Range(0f, 30f)]
     private int reach;
     public int Reach { get {  return reach; } }
 }
