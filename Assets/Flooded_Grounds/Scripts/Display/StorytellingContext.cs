7feec0a37de6f8e30893a094d54d6baaa1daad1b
Luca <zzzzqqqw@naver.com>
2024-02-01 19:13:15 +0900

    add Scene_Storytelling button interactions

--- Assets/Flooded_Grounds/Scripts/Display/StorytellingContext.cs
+++ Assets/Flooded_Grounds/Scripts/Display/StorytellingContext.cs
@@ -1,33 +1,57 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
+using System.Runtime.CompilerServices;
+using Unity.Linq;
 
 public class StorytellingContext : MonoBehaviour
 {
     public TMP_Text Text;
     public TMP_Text TextShadow;
-    string context;
+    GameObject ButtonMove;
+    GameObject ButtonNext;
 
-    public string[] contextArray; 
+    public string[] contextArray;
+    private int currentIndex = 0;
 
     private void Start()
     {
-        context = "게임 시작 스토리텔링 설명 구간\r\n어쩌구 마을 침수가 어쩌구\r\n위험하네 뭐네 호기심\r\n구하러 감";
-        StartCoroutine(Show(context));
+        StartCoroutine(Show(contextArray[currentIndex]));
+        currentIndex++;
+        ButtonNext = GameObject.Find("Canvas/Background/ButtonNext").gameObject;
+        ButtonMove = GameObject.Find("Canvas/Background/ButtonMove").gameObject;
+        ButtonMove.SetActive(false);
     }
-    IEnumerator Show(string dialog)
+
+    public void ButtonNext_clicked()
+    {
+        if (currentIndex < contextArray.Length)
+        {
+            StartCoroutine(Show(contextArray[currentIndex]));
+            currentIndex++;
+        }
+        else if (currentIndex == contextArray.Length)
+        {
+            ChangeButton();
+        }
+    }
+
+    IEnumerator Show(string context)
     {
         Text.text = string.Empty;
         TextShadow.text = string.Empty;
 
-        for (int i = 0; i < dialog.Length; i++)
+        for (int i = 0; i < context.Length; i++)
         {
-            Text.text += dialog[i];
-            TextShadow.text += dialog[i];
-            yield return new WaitForSeconds(0.1f);
-
+            Text.text += context[i];
+            TextShadow.text += context[i];
+            yield return new WaitForSeconds(0.06f);
         }
+    }
+
+    private void ChangeButton()
+    {
+        ButtonNext.SetActive(false);
+        ButtonMove.SetActive(true);
     }
 }
