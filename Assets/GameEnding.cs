1d17666da729099bd1a5774abb23d18d904cab92
현진혁 <73646152+guswls4928@users.noreply.github.com>
2024-02-08 19:07:05 +0900

    car event

--- Assets/GameEnding.cs
+++ Assets/GameEnding.cs
@@ -4,35 +4,19 @@ using UniRx.Triggers;
 
 public class GameEnding : MonoBehaviour
 {
-    public GameObject GameEndingObj;
-    public bool isOnCollision = false;
-
-    public GameObject GameEndingUI;
-    public static bool GameIsPaused = false;
+    [SerializeField]
+    private GameObject GameEndingUI;
 
     private void Start()
     {
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
-
-        this.UpdateAsObservable()
-            .Where(_ => isOnCollision == true)
-            .Subscribe(_ => GameState());
-    }
-
-    public void GameState()
-    {
-        GameIsPaused = !GameIsPaused;
-        Time.timeScale = (Time.timeScale + 1) % 2;
-        Cursor.visible = GameIsPaused;
-        Cursor.lockState = (CursorLockMode)(((int)Cursor.lockState + 1) % 2);
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            isOnCollision = true;
-        }
+        this.OnTriggerEnterAsObservable()
+            .Subscribe(_ =>
+            {
+                Pause.GameIsPaused = !Pause.GameIsPaused;
+                Time.timeScale = (Time.timeScale + 1) % 2;
+                Cursor.visible = Pause.GameIsPaused;
+                Cursor.lockState = (CursorLockMode)(((int)Cursor.lockState + 1) % 2);
+                GameEndingUI.SetActive(true);
+            });
     }
 }
