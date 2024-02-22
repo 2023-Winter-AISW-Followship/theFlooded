2ee97910c5c11b85302289c612422efb7f6938d1
현진혁 <73646152+guswls4928@users.noreply.github.com>
2024-02-22 17:21:44 +0900

    ending pause bug fix

--- Assets/Flooded_Grounds/Scripts/Setting/Pause/Pause.cs
+++ Assets/Flooded_Grounds/Scripts/Setting/Pause/Pause.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using UniRx;
 using UniRx.Triggers;
 using Unity.Linq;
@@ -8,6 +9,7 @@ public class Pause : MonoBehaviour
     public GameObject PauseMenu;
     public static bool GameIsPaused = false;
     private GameObject settingCanvas;
+    private GameObject endCanvas;
 
     private void Start()
     {
@@ -15,6 +17,7 @@ public class Pause : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked;
 
         settingCanvas = GameObject.Find("KeySetting");
+        endCanvas = gameObject.Descendants().Where(x => x.name.Equals("GameEnding")).FirstOrDefault().gameObject;
 
         this.UpdateAsObservable()
             .Where(_ => Input.GetKeyDown(KeyCode.Escape))
@@ -28,6 +31,7 @@ public class Pause : MonoBehaviour
         Cursor.visible = GameIsPaused;
         Cursor.lockState = (CursorLockMode)(((int)Cursor.lockState + 1) % 2);
         PauseMenu.SetActive(GameIsPaused);
+        endCanvas.SetActive(false);
     }
 
     public void Setting()
